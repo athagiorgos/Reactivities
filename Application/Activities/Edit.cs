01a1ae55c5defd98564dc565c0ba33b2b3db52ae
athagiorgod <athan711@gmail.com>
2021-08-09 13:11:27 +0300

    Added AutoMapper and Edit operation support

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,9 @@
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
+using Persistence;
 
 namespace Application.Activities
 {
@@ -8,6 +12,30 @@ namespace Application.Activities
         public class Command : IRequest
         {
             public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                // Mapping the activity object from the request, with the activity object from the database.
+                _mapper.Map(request.Activity, activity);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
         }
     }
 }
