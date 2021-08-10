3f5ea3a606265c375b3c22749ecc55b01ef00f17
athagiorgod <athan711@gmail.com>
2021-08-10 11:34:54 +0300

    NavBar and ActivityDashboard in react

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
 
@@ -18,17 +20,14 @@ function App() {
 
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
+    <Fragment>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+       <ActivityDashboard activities={activities} />
+      </Container>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
-    </div>
+        
+    </Fragment>
   );
 }
 
