2bbe531dcaa117610ed3c2f5c92786711ac118de
athagiorgod <athan711@gmail.com>
2021-08-10 14:21:25 +0300

    View Activities functionality

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,44 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
-import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
-import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
-
   // return the value(current state) and a function to set the value again
   // using a type to useState function
-  const[activities, setActivities] = useState<Activity[]>([]);
-
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
-    })
-  }, [])
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
 
+  function handleSelectedActivity(id: string) {
+    setSelectedActivity(activities.find((act) => act.id));
+  }
+
+  function handleCandelSelectedActivity() {
+    setSelectedActivity(undefined);
+  }
 
   return (
     <Fragment>
       <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard activities={activities} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectedActivity}
+          cancelSelectActivity={handleCandelSelectedActivity}
+        />
       </Container>
     </Fragment>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,29 +1,40 @@
-import React from 'react';
-import { Grid, GridColumn } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
-import ActivityList from './ActivityList';
+import React from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
 
 // This gives us type safety
 // destructuring the properties that we pass down from the activity dashboard
 interface Props {
-    activities: Activity[];
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
 }
 
-
-export default function ActivityDashboard({activities}: Props) {
-    return (
-        <Grid>
-            <Grid.Column width='10'>
-                <ActivityList activities={activities} />
-            </Grid.Column>
-            <GridColumn width='6'>
-                {/* if the activity exists */}
-                {activities[0] &&
-                <ActivityDetails activity={activities[0]} />}
-                <ActivityForm />
-            </GridColumn>
-        </Grid>
-    )
+export default function ActivityDashboard({
+  activities,
+  selectedActivity,
+  selectActivity,
+  cancelSelectActivity,
+}: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} selectActivity={selectActivity} />
+      </Grid.Column>
+      <GridColumn width="6">
+        {/* if the activity exists */}
+        {selectedActivity && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+          />
+        )}
+        <ActivityForm />
+      </GridColumn>
+    </Grid>
+  );
 }
