572406e9f01735fe4b876546af579c8847f2f872
athagiorgod <athan711@gmail.com>
2021-08-16 15:53:48 +0300

    Routing in React

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,21 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from "../stores/store";
 
 export default function NavBar() {
-  const { activityStore } = useStore();
-
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} to="/" exact header>
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activitites" />
+        <Menu.Item as={NavLink} to="/activities" name="Activitites" />
         <Menu.Item>
           <Button
-            onClick={() => activityStore.openForm()}
+            as={NavLink}
+            to="/createActivity"
             positive
             content="Create Activity"
           />
