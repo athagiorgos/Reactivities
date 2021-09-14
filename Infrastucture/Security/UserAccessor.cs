b2822447a4def7d8957e2cca6f4548bfd02fdc41
athagiorgod <athan711@gmail.com>
2021-09-14 13:24:45 +0300

    Entity Framework Relationships

---
+++ Infrastucture/Security/UserAccessor.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastucture.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUsername()
+        {
+            // getting user name from HttpContext with the use of _httpContextAccessor interface
+            return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}
