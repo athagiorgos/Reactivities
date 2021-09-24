1b848086fad6b7e9d6bafdc915b6bda3b5209bd6
athagiorgos <athan711@gmail.com>
2021-09-24 16:00:43 +0300

    Followers and Following feature, bug fixing

--- ClientApp/src/app/stores/profileStore.ts
+++ ClientApp/src/app/stores/profileStore.ts
@@ -1,5 +1,5 @@
 ﻿import {Photo, Profile} from "../models/profile";
-import {makeAutoObservable, runInAction} from "mobx";
+import {makeAutoObservable, reaction, runInAction} from "mobx";
 import agent from "../api/agent";
 import {store} from "./store";
 
@@ -9,12 +9,28 @@ export default class ProfileStore {
     uploading = false;
     loading = false;
     followings: Profile[] = [];
+    loadingFollowings = false;
+    activeTab = 0;
     
     constructor() {
         makeAutoObservable(this);
+
+        reaction(
+            () => this.activeTab,
+            activeTab => {
+                if (activeTab === 3 || activeTab === 4) {
+                    const predicate = activeTab === 3 ? 'followers' : 'following';
+                    this.loadFollowings(predicate);
+                } else {
+                    this.followings = [];
+                }
+            }
+        )
     }
-    
-    
+
+    setActiveTab = (activeTab: any) => {
+        this.activeTab = activeTab;
+    }
     get isCurrentUser() {
         if (store.userStore.user && this.profile) {
             return store.userStore.user.username === this.profile.username;
@@ -136,5 +152,15 @@ export default class ProfileStore {
             runInAction(() => this.loading = false)
         }
     }
+    
+   loadFollowings = async (predicate: string) => {
+        this.loadingFollowings = true;
+        try {
+            const followings = await agent.Profiles.listFollowings(this.profile!.username, predicate);
+        } catch (error) {
+            console.log(error);
+            runInAction(() => this.loadingFollowings = false)
+        }
+   }
 
 }
