7eb46be7372e91d2b3901a4e7aef458053cc593c
athagiorgod <athan711@gmail.com>
2021-09-28 13:03:42 +0300

    Added user activities view in user profile

--- ClientApp/src/app/api/agent.ts
+++ ClientApp/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import { history } from "../..";
 import {Activity, ActivityFormValues} from "../models/activity";
 import { User, UserFormValues } from "../models/user";
 import { store } from "../stores/store";
-import {Photo, Profile} from "../models/profile";
+import {Photo, Profile, UserActivity} from "../models/profile";
 import {PaginatedResult} from "../models/pagination";
 
 const sleep = (delay: number) => {
@@ -109,7 +109,9 @@ const Profiles = {
   deletePhoto: (id: string) => requests.del(`/photos/${id}`),
   updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile),
   updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
-  listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+  listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+  listActivities: (username: string, predicate: string) => 
+      requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 const agent = {
